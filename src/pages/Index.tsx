a965ff90eb28be146d1e58d7fe009710b4130d53
Keegan Elliott <keegan@robotire.com>
2023-12-07 13:52:43 -0500

    fixed import

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,15 @@
-import { React } from 'react'
+import React from 'react';
 import '../styles/index.css'
 
 function Index() {
 
   return (
     <>
-      <a href='https://www.linkedin.com/in/keeganelliott/' target='_blank'>
+      <a href='https://www.linkedin.com/in/keeganelliott/' target='_blank' rel="noreferrer">
         <h1 className='me text-center align-middle font-mono'>Keegan Elliott</h1>
       </a>
     </>
   )
 }
 
-export default Index
+export default Index;
